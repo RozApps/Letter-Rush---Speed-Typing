9b7ca8eec7459d8f8e61827ae98bde02440fcf29
RozApps <rozkia@gmail.com>
2025-08-13 14:21:02 -1000

    Update wordlists.js

--- wordlists.js
+++ wordlists.js
@@ -1,35 +1,111 @@
-// Topics + Word lists in a separate file for easy updates
-window.TOPICS = [
-  'Animals','Countries','Fruits','Cities','Colors','Sports','Vehicles',
-  'Occupations','Things in a kitchen','Movies','TV Shows','Cartoons',
-  'Superheroes','Foods','Desserts','Body parts','School subjects','Toys',
-  'Games','Stores','Restaurants','Holidays','Tools','Weather','Emotions'
-];
+// wordlists.js (5 topics, expanded; keeps existing entries and adds more)
+window.TOPICS = ['Animals','Foods','Games','Cartoons','Occupations'];
+window.WORD_LISTS = window.WORD_LISTS || {};
 
-window.WORD_LISTS = {
-  'Animals': ['aardvark','alligator','alpaca','ant','anteater','antelope','ape','armadillo','baboon','badger','barn owl','bat','bear','beaver','bee','beetle','bird','bison','black bear','blue whale','boar','brown bear','buffalo','bull','butterfly','camel','cat','caterpillar','cheetah','chicken','chimpanzee','chipmunk','cobra','cow','coyote','crab','crane','crocodile','crow','deer','dog','dolphin','donkey','dove','duck','eagle','eel','elephant','elk','emu','falcon','ferret','fire ant','fish','flamingo','fly','fox','frog','fruit bat','gazelle','giraffe','goat','goose','gorilla','grasshopper','gray wolf','grizzly bear','hamster','hawk','hedgehog','hippo','honey bee','horse','hummingbird','hyena','iguana','jackal','jaguar','jellyfish','kangaroo','killer whale','koala','ladybug','lamb','leopard','lion','lizard','llama','lobster','lynx','monkey','moose','mouse','mule','newt','octopus','opossum','ostrich','otter','owl','ox','panda','panther','parrot','peacock','pelican','penguin','pig','pigeon','polar bear','porcupine','quail','rabbit','raccoon','rat','raven','red fox','rhinoceros','robin','sea lion','seahorse','seal','shark','sheep','skunk','sloth','snail','snake','spider','squirrel','starfish','swan','tiger','turkey','turtle','unicorn','vampire bat','vulture','walrus','wasp','whale','white shark','wolf','woodpecker','worm','yak','zebra'],
-  'Countries': ['afghanistan','albania','algeria','andorra','angola','argentina','armenia','australia','austria','azerbaijan','bahamas','bahrain','bangladesh','barbados','belarus','belgium','belize','benin','bhutan','bolivia','bosnia','botswana','brazil','brunei','bulgaria','burkina faso','burundi','cambodia','cameroon','canada','cape verde','chad','chile','china','colombia','comoros','congo','costa rica','croatia','cuba','cyprus','czech republic','democratic republic of congo','denmark','djibouti','dominica','dominican republic','east timor','ecuador','egypt','el salvador','equatorial guinea','eritrea','estonia','ethiopia','fiji','finland','france','gabon','gambia','georgia','germany','ghana','greece','grenada','guatemala','guinea','guinea bissau','guyana','haiti','honduras','hungary','iceland','india','indonesia','iran','iraq','ireland','israel','italy','ivory coast','jamaica','japan','jordan','kazakhstan','kenya','kiribati','kuwait','kyrgyzstan','laos','latvia','lebanon','lesotho','liberia','libya','liechtenstein','lithuania','luxembourg','macedonia','madagascar','malawi','malaysia','maldives','mali','malta','marshall islands','mauritania','mauritius','mexico','micronesia','moldova','monaco','mongolia','montenegro','morocco','mozambique','myanmar','namibia','nauru','nepal','netherlands','new zealand','nicaragua','niger','nigeria','north korea','norway','oman','pakistan','palau','panama','papua new guinea','paraguay','peru','philippines','poland','portugal','qatar','romania','russia','rwanda','saint lucia','samoa','san marino','saudi arabia','senegal','serbia','seychelles','sierra leone','singapore','slovakia','slovenia','solomon islands','somalia','south africa','south korea','south sudan','spain','sri lanka','sudan','suriname','swaziland','sweden','switzerland','syria','taiwan','tajikistan','tanzania','thailand','togo','tonga','trinidad and tobago','tunisia','turkey','turkmenistan','tuvalu','uganda','ukraine','united arab emirates','united kingdom','united states','uruguay','uzbekistan','vanuatu','vatican city','venezuela','vietnam','wales','yemen','zambia','zimbabwe'],
-  'Fruits': ['acai','apple','apricot','avocado','banana','blackberry','blackcurrant','blueberry','boysenberry','breadfruit','cantaloupe','cherry','clementine','coconut','cranberry','currant','date','dragonfruit','durian','elderberry','fig','gooseberry','grape','grapefruit','guava','honeydew','huckleberry','jackfruit','kiwi','kumquat','lemon','lime','lingonberry','lychee','mandarin','mango','melon','mulberry','nectarine','orange','papaya','passionfruit','peach','pear','persimmon','pineapple','plum','pomegranate','pomelo','prickly pear','quince','rambutan','raspberry','starfruit','strawberry','tangerine','tomato','ugli fruit','vanilla','watermelon','ximenia','yuzu','zucchini'],
-  'Colors': ['amber','aqua','aquamarine','azure','beige','black','blue','bronze','brown','burgundy','burnt orange','chartreuse','coral','crimson','cyan','dark blue','emerald','forest green','fuchsia','gold','gray','green','hot pink','indigo','ivory','jade','khaki','lavender','lemon','lime','magenta','maroon','mint','navy','olive','orange','peach','pink','plum','purple','red','rose','ruby','salmon','scarlet','silver','tan','teal','turquoise','ultramarine','violet','white','yellow','zinc'],
-  'Cities': ['atlanta','amsterdam','austin','baghdad','baltimore','bangkok','barcelona','beijing','berlin','birmingham','bogota','boston','brussels','budapest','buenos aires','cairo','calcutta','cape town','caracas','chicago','cologne','copenhagen','dallas','delhi','denver','detroit','dublin','dubai','edinburgh','florence','frankfurt','geneva','glasgow','hamburg','helsinki','hong kong','houston','istanbul','jakarta','jerusalem','johannesburg','kansas city','kiev','kuala lumpur','kyoto','las vegas','lima','lisbon','london','los angeles','lyon','madrid','manchester','manila','marseille','melbourne','mexico city','miami','milan','montreal','moscow','mumbai','munich','nairobi','naples','new delhi','new york','nice','oslo','ottawa','paris','philadelphia','phoenix','prague','quebec','rio de janeiro','rome','saint petersburg','san francisco','santiago','sao paulo','seattle','seoul','shanghai','singapore','stockholm','sydney','tehran','tokyo','toronto','tunis','vancouver','venice','vienna','warsaw','washington','yokohama','zurich'],
-  'Sports': ['archery','athletics','badminton','baseball','basketball','biathlon','bobsled','bowling','boxing','canoeing','cricket','curling','cycling','darts','diving','equestrian','fencing','football','golf','gymnastics','handball','hockey','judo','karate','kayaking','lacrosse','marathon','martial arts','netball','polo','powerlifting','quidditch','racquetball','rowing','rugby','running','sailing','skiing','snowboarding','soccer','softball','squash','surfing','swimming','table tennis','taekwondo','tennis','track and field','triathlon','ultimate frisbee','volleyball','water polo','weightlifting','wrestling','yoga'],
-  'Vehicles': ['airplane','ambulance','bicycle','boat','bus','car','canoe','drone','ferry','helicopter','jet','kayak','limousine','motorcycle','plane','rocket','scooter','ship','submarine','taxi','train','truck','van','yacht'],
-  'Occupations': ['actor','baker','chef','doctor','engineer','farmer','guard','nurse','pilot','teacher','writer'],
-  'Things in a kitchen': ['blender','bowl','cup','dish','fork','glass','kettle','knife','ladle','mug','oven','pan','plate','pot','spoon','toaster'],
-  'Movies': ['avatar','batman','cars','dumbo','elf','frozen','gladiator','hulk','inception','jaws','kramer','lego movie','matrix','nemo','pirates','rocky','superman','titanic','up','wall-e','x-men'],
-  'TV Shows': ['friends','seinfeld','the office','breaking bad','lost','game of thrones','stranger things','the simpsons','family guy','south park'],
-  'Cartoons': ['mickey mouse','bugs bunny','tom and jerry','scooby doo','spongebob','pokemon','dragon ball','naruto','sailor moon','teenage mutant ninja turtles'],
-  'Superheroes': ['batman','superman','spiderman','wonder woman','iron man','captain america','hulk','thor','flash','green lantern','aquaman','wolverine','deadpool','black widow','ant-man'],
-  'Foods': ['apple','bread','cheese','donut','egg','fish','garlic','ham','ice cream','jam','kale','lobster','meat','nuts','onion','pasta','quinoa','rice','soup','toast','udon','vanilla','waffle','yogurt','ziti'],
-  'Desserts': ['cake','cookies','pie','ice cream','pudding','brownie','donut','candy','chocolate','marshmallow','fudge','cupcake','tart','mousse','sorbet'],
-  'Body parts': ['ankle','arm','back','chest','ear','elbow','eye','face','foot','hand','head','knee','leg','mouth','neck','nose','shoulder','thumb','toe','waist'],
-  'School subjects': ['art','biology','chemistry','drama','english','french','geography','history','music','physics','science','math','literature','psychology','sociology'],
-  'Toys': ['ball','doll','blocks','puzzle','teddy bear','car','train','airplane','robot','action figure','board game','video game','kite','yo-yo','frisbee'],
-  'Games': ['chess','checkers','monopoly','scrabble','poker','blackjack','bingo','solitaire','hide and seek','tag','hopscotch','jump rope','video games','board games','card games'],
-  'Stores': ['grocery store','pharmacy','bookstore','clothing store','hardware store','pet store','toy store','electronics store','department store','convenience store'],
-  'Restaurants': ['mcdonalds','burger king','subway','pizza hut','kfc','taco bell','chinese restaurant','italian restaurant','mexican restaurant','fast food','diner','cafe'],
-  'Holidays': ['christmas','thanksgiving','halloween','easter','valentines day','new years','independence day','memorial day','labor day','mothers day','fathers day','birthday'],
-  'Tools': ['hammer','screwdriver','wrench','saw','drill','pliers','measuring tape','level','chisel','file','sandpaper','nail','screw','bolt','nut'],
-  'Weather': ['sunny','cloudy','rainy','snowy','windy','stormy','foggy','humid','dry','hot','cold','freezing','warm','cool','thunder','lightning','hail','sleet','drizzle','mist'],
-  'Emotions': ['happy','sad','angry','excited','nervous','calm','surprised','confused','proud','embarrassed','jealous','grateful','worried','relaxed','frustrated','content','anxious','joyful','disappointed','hopeful']
-};
+// Helper: merge while preserving your original order, removing duplicates
+function mergeList(topic, extra) {
+  const original = window.WORD_LISTS[topic] || [];
+  const merged = [...new Set([...original, ...extra])];
+  window.WORD_LISTS[topic] = merged;
+}
+
+// === ANIMALS (additions) ===
+mergeList('Animals', [
+  'aardwolf','addax','albatross','anaconda','angelfish','anglerfish','antbird','antshrike','axolotl',
+  'babirusa','bald eagle','bandicoot','barracuda','basilisk lizard','beetle larva','beluga','binturong',
+  'blackbird','blue jay','boa','bobcat','bonobo','bongo','burro',
+  'capybara','caracal','cassowary','centipede','chameleon','cicada','civet','coati','condor','cougar',
+  'cuttlefish','damselfish','degu','dik-dik','dingo','dodo','dormouse','dromedary',
+  'echidna','egret','eland','ermine','eurasian lynx',
+  'falabella','fallow deer','fiddler crab','finch','firefly','fisher cat','flounder','fossa','fox terrier','fruit fly',
+  'gaur','gazelle','gecko','gerbil','gharial','gibbon','gila monster','goblin shark','goldfinch','goosefish',
+  'harp seal','hartebeest','heron','hornbill','horseshoe crab','howler monkey','humpback whale',
+  'ibex','ibis','impala','jackrabbit','jacana','jay','jerboa','kakapo','kangaroo rat','katydid',
+  'kestrel','kinkajou','kite','kiwi bird','klipspringer','kookaburra','kouprey',
+  'lemming','lemur','lionfish','loris','lyrebird','macaque','magpie','manatee','mandrill','markhor',
+  'marten','meerkat','mink','mole','mola mola','mongoose','myna','nautilus','narwhal','nighthawk',
+  'nudibranch','ocelot','okapi','opossum shrimp','oriole','oryx','oyster','pangolin','peccary','peregrine falcon',
+  'pheasant','pika','platypus','porpoise','ptarmigan','pufferfish','puma','python',
+  'quetzal','quokka','quoll','ray','red panda','reindeer','roadrunner',
+  'saiga','salamander','sandpiper','sardine','sea urchin','serval','shoebill','silkworm','spoonbill','squid','starling','stingray',
+  'sugar glider','swift','tamarin','tapir','tarantula','tasmanian devil','tern','thorny devil','thrush','toucan',
+  'tuatara','tuna','uakari','urial','vicuna','viper','walleye','warthog','weasel','whip-poor-will',
+  'wildebeest','wobbegong','wolverine','woodlouse','yakutian horse','yellowhammer','zebu'
+]);
+
+// === FOODS (additions) ===
+mergeList('Foods', [
+  'bagel','baguette','baked potato','barbecue','bbq ribs','bento','bibimbap','biscuit','bisque','burrito',
+  'cabbage rolls','cacio e pepe','calzone','cannoli','caprese salad','carne asada','casserole','ceviche','cheeseburger',
+  'cheesesteak','chimichanga','chili','chips and salsa','chow mein','clam chowder','club sandwich','collard greens',
+  'couscous','crab cake','croissant','curry','dal','deviled eggs','dumplings','edamame','egg rolls','empanada',
+  'enchiladas','fajitas','falafel','fried chicken','fried rice','frittata','garlic bread','gelato','gnocchi',
+  'greek salad','grilled cheese','grits','gumbo','gyoza','gyro','halibut','ham sandwich','hummus','ice cream sandwich',
+  'jambalaya','jerk chicken','kebab','kimchi','korean bbq','laksa','lasagna','lobster roll','mac and cheese','macaroni salad',
+  'maki','mapo tofu','mashed potatoes','meatballs','meatloaf','miso soup','mochi','moussaka','muffin','nachos',
+  'nigiri','noodles','oatmeal','omelet','onion rings','pad thai','paella','pakora','pancakes','paneer tikka',
+  'panzanella','parmigiana','peking duck','pho','pierogi','pita','pizzetta','poached eggs','poke bowl','polenta',
+  'pork chops','pot pie','quesadilla','quiche lorraine','ramen','ratatouille','refried beans','risotto','roast beef','roasted veggies',
+  'samosa','sandwich','sashimi','sausage','shawarma','shepherds pie','shrimp scampi','smoothie','soba','souvlaki',
+  'spaghetti','spring rolls','steak','stew','stir fry','stuffed peppers','succotash','sushi','tacos','tamagoyaki',
+  'tamales','tempura','teriyaki','tikka masala','tiramisu','toast points','tofu','tortilla soup','tuna melt','udon bowl',
+  'veggie burger','waffles','wild rice','yakisoba','yukon fries','ziti bake'
+]);
+
+// === GAMES (additions) ===
+mergeList('Games', [
+  'uno','uno flip','phase 10','skip-bo','go','mahjong','yahtzee','rummy','canasta','euchre','hearts','spades','gin rummy',
+  'old maid','war','crazy eights','skat','bridge','dominoes','backgammon','go fish','connect four','battleship','risk','clue',
+  'catan','ticket to ride','carcassonne','pandemic','azul','splendor','dixit','codenames','taboo','pictionary','charades',
+  'twister','jenga','boggle','trivial pursuit','guess who','operation','kerplunk','mancala','ouija','tic tac toe','paper football',
+  'hangman','simon says','red light green light','four square','marbles','jacks','kick the can','capture the flag','duck duck goose',
+  'musical chairs','freeze tag','telephone game','truth or dare','spin the bottle','werewolf','one night werewolf','mafia','avalon',
+  'exploding kittens','bananagrams','monopoly deal','wordle','crossword','sudoku','kakuro','nonogram','minesweeper','snake',
+  'tetris','pac-man','space invaders','donkey kong','street fighter','mortal kombat','mario kart','super smash bros','zelda',
+  'pokemon','animal crossing','minecraft','roblox','fortnite','apex legends','call of duty','overwatch','league of legends','dota',
+  'valorant','hearthstone','starcraft','rocket league','fifa','nba 2k','madden','just dance','guitar hero','beat saber',
+  'geo guesser','portal','bioshock','halo','diablo','the sims'
+]);
+
+// === CARTOONS (additions) ===
+mergeList('Cartoons', [
+  'adventure time','regular show','steven universe','gravity falls','avatar the last airbender','the legend of korra',
+  'phineas and ferb','kim possible','the owl house','amphibia','bluey','peppa pig','paw patrol','dora the explorer','go diego go',
+  'caillou','arthur','curious george','looney tunes','tiny toons','animaniacs','the fairly oddparents','danny phantom',
+  'hey arnold','rugrats','doug','rockos modern life','catdog','johnny bravo','the powerpuff girls','samurai jack',
+  'dexters laboratory','ben 10','fosters home for imaginary friends','the amazing world of gumball','chowder',
+  'home movies','south park','family guy','american dad','king of the hill','rick and morty','bojack horseman','big mouth',
+  'bobs burgers','disenchantment','star vs the forces of evil','she-ra and the princesses of power','voltron legendary defender',
+  'teen titans','teen titans go','young justice','x-men the animated series','batman the animated series','spider-man the animated series',
+  'gargoyles','darkwing duck','ducktales','chip n dale rescue rangers','tailspin','recess','teacher’s pet','the weekenders',
+  'code lyoko','winx club','sailor moon crystal','dragon ball z','dragon ball super','naruto shippuden','one piece','bleach',
+  'my hero academia','demon slayer','attack on titan','fullmetal alchemist brotherhood','cowboy bebop','yu-gi-oh','beyblade',
+  'cardcaptor sakura','inuyasha','fairy tail','sailor moon r','pokemon journeys','digimon'
+]);
+
+// === OCCUPATIONS (additions) ===
+mergeList('Occupations', [
+  'accountant','actor','actuary','administrator','air traffic controller','analyst','animator','archaeologist','architect',
+  'artist','astronaut','attorney','audio engineer','baker','banker','barber','bartender','biologist','bookkeeper',
+  'bricklayer','bus driver','butcher','cab driver','carpenter','cashier','chef','chemical engineer','chemist','chiropractor',
+  'civil engineer','coach','construction worker','consultant','content creator','cook','copywriter','court reporter',
+  'customer service representative','data analyst','data engineer','data scientist','dental hygienist','dentist','designer',
+  'developer','dietitian','dispatcher','doctor','driver','electrician','emergency medical technician','event planner',
+  'farmer','farrier','fashion designer','filmmaker','financial advisor','firefighter','fisherman','flight attendant','florist',
+  'game designer','gardener','geologist','graphic designer','hair stylist','housekeeper','hr manager','interpreter','janitor',
+  'journalist','judge','landscaper','lawyer','lecturer','librarian','lifeguard','line cook','locksmith','machine operator',
+  'mail carrier','maintenance worker','marketing manager','massage therapist','mechanic','medical assistant','meteorologist',
+  'midwife','miner','musician','network engineer','nutritionist','occupational therapist','office manager','optometrist',
+  'painter','paramedic','park ranger','pharmacist','photographer','physical therapist','physician','physicist','pilot',
+  'plumber','police officer','politician','postal worker','principal','producer','product manager','programmer',
+  'project manager','psychiatrist','psychologist','publicist','quality assurance tester','real estate agent','receptionist',
+  'researcher','sailor','salesperson','scientist','security guard','social worker','software engineer','soldier',
+  'speech therapist','statistician','surgeon','taxi driver','teacher','teacher aide','technician','therapist','translator',
+  'truck driver','tutor','uber driver','veterinarian','videographer','waiter','waitress','welder','writer','yoga instructor',
+  'zookeeper'
+]);
+
+// Optional: ensure each topic exists even if the original file didn’t have it
+['Animals','Foods','Games','Cartoons','Occupations'].forEach(t => {
+  window.WORD_LISTS[t] = window.WORD_LISTS[t] || [];
+});
